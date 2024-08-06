48399eee136ef7af379924e6633474bf67874e19
kwon <wingseter@gmail.com>
2024-08-06 14:32:53 +0000

    [fix] fix file path for linux environment

--- DataDownload.sh
+++ DataDownload.sh
@@ -1,5 +1,7 @@
+apt-get upgrade && apt-get update
 pip install kaggle
 mkdir Data
 kaggle competitions download -c sartorius-cell-instance-segmentation
+apt-get install unzip -y
 unzip sartorius-cell-instance-segmentation.zip -d Data
 rm -f sartorius-cell-instance-segmentation.zip
